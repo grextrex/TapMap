20e4c4f4b0b1f5ad5efc584fd58814f9c8209ee3
Raj SHekhar <raj@shekharpro.com>
2016-05-05 12:05:34 +0530

    Initial commit

---
+++ app/src/main/java/com/example/shekhar/tapmap/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.shekhar.tapmap;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
